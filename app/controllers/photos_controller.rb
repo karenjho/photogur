8a52f12811cf495e097865f542cc6fbaddaf38c1
karenjho <hokarenj@gmail.com>
2016-01-26 16:36:49 -0500

    Add resources to routes and create corresponding 7 methods in controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,45 @@
 class PhotosController < ApplicationController
+
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+  end
+
+  private
+
+  def photo_params
+    params.require(:photos).permit(:artist, :title, :year, :url)
+  end
 end
